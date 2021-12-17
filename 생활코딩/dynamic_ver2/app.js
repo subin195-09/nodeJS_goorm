7087b3fa0ee48b62ad20f657d9b81791aecabb03
subin195-09 <k_subin@naver.com>
2021-12-17 23:55:18 +0900

    12/17 pm2 setting & add /update

--- 생활코딩/dynamic_ver2/app.js
+++ 생활코딩/dynamic_ver2/app.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, description) {
+function templateHTML(title, list, description, control) {
   return `<!doctype html>
   <html>
   <head>
@@ -12,7 +13,7 @@ function templateHTML(title, list, description) {
   <body>
 	<h1><a href="/">WEB</a></h1>
 	${list}
-	<a href="/create">create</a>
+	${control}
 	<h2>${title}</h2>
 	<p>
 	${description}
@@ -27,10 +28,11 @@ var app = http.createServer(function(request, response){
 	var queryData = url.parse(_url, true).query;
 	var pathname = url.parse(_url, true).pathname;
 	var title = queryData.id;
+	var contral = _url === '/' ? '<a href="/create">create</a>' : `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`;
 
 	var list = '<ul>';
 	fs.readdir('./data/', function (err, filelist) {
-		console.log(filelist);
+		// console.log(filelist);
 		filelist.forEach(function (data) {
 			if (data !== 'welcome')
 				list += '<li><a href="/?id=' + data + '">' + data + '</a></li>';
@@ -38,20 +40,19 @@ var app = http.createServer(function(request, response){
 	});
 	list += '</ul>';
 
-	if (pathname == '/') {
-		if (_url == '/')
+	if (pathname === '/') {
+		if (_url === '/')
 			title = 'welcome';
 
 		fs.readFile(`data/${title}`, 'utf8', function(err, description){
-			var template = templateHTML(title, list, description);
+			var template = templateHTML(title, list, description, contral);
 			response.writeHead(200);
 			response.end(template);
 		});
-	} else if (pathname == '/create') {
-		console.log('create');
+	} else if (pathname === '/create') {
 		var title = 'WEB - create';
 		var template = templateHTML(title, list, `
-		<form action="http://localhost:3000/process_create" method="post">
+		<form action="/create_process" method="post">
 			<p><input type="text" name="title" placeholder="title"></p>
 			<p>
 				<textarea name="description" placeholder="description"></textarea>
@@ -59,9 +60,56 @@ var app = http.createServer(function(request, response){
 			<p>
 				<input type="submit">
 			</p>
-		</form>`);
+		</form>`, '');
 		response.writeHead(200);
 		response.end(template);
+	} else if (pathname === '/create_process') {
+		var body = '';
+		request.on('data', function(data){
+			body += data;
+		});
+		request.on('end', function(data){
+			var post = qs.parse(body);
+			var title = post.title;
+			var description = post.description;
+			fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+				response.writeHead(302, {Location: `/?id=${title}`});
+				response.end();
+			});
+		});
+	} else if (pathname === '/update') {
+		fs.readFile(`data/${title}`, 'utf8', function(err, description){
+			var template = templateHTML(title, list, `
+			<form action="/update_process" method="post">
+				<input type="hidden" name="id" value="${title}">
+				<p><input type="text" name="title" placeholder="title" value=${title}></p>
+				<p>
+					<textarea name="description" placeholder="description">${description}</textarea>
+				</p>
+				<p>
+					<input type="submit">
+				</p>
+			</form>`, '');
+			response.writeHead(200);
+			response.end(template);
+		});
+	} else if (pathname === '/update_process') {
+		var body = '';
+		request.on('data', function(data){
+			body += data;
+		});
+		request.on('end', function(data){
+			var post = qs.parse(body);
+			var id = post.id;
+			var title = post.title;
+			var description = post.description;
+			fs.rename(`data/${id}`, `data/${title}`, function(err){
+				fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+					response.writeHead(302, {Location: `/?id=${title}`});
+					response.end();
+				});
+			});
+		});
 	} else {
 		response.writeHead(404);
 		response.end("Not found");
