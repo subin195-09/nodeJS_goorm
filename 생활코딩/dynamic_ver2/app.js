3c234c27d6842c1450aa64b50f67af909b2afd78
subin195-09 <k_subin@naver.com>
2021-12-17 23:18:51 +0900

    12/17 pm2 setting & add /creat

--- 생활코딩/dynamic_ver2/app.js
+++ 생활코딩/dynamic_ver2/app.js
@@ -12,6 +12,7 @@ function templateHTML(title, list, description) {
   <body>
 	<h1><a href="/">WEB</a></h1>
 	${list}
+	<a href="/create">create</a>
 	<h2>${title}</h2>
 	<p>
 	${description}
@@ -46,19 +47,21 @@ var app = http.createServer(function(request, response){
 			response.writeHead(200);
 			response.end(template);
 		});
-	} else if (pathname == '/creat') {
-		fs.readdir('./data/', function (err, filelist) {
-			var title = 'WEB - create';
-			list = '<ul>';
-			fs.readdir('./data/', function (err, filelist) {
-				console.log(filelist);
-				filelist.forEach(function (data) {
-					if (data !== 'welcome')
-						list += '<li><a href="/?id=' + data + '">' + data + '</a></li>';
-				});
-			});
-			list += '</ul>';
-		});
+	} else if (pathname == '/create') {
+		console.log('create');
+		var title = 'WEB - create';
+		var template = templateHTML(title, list, `
+		<form action="http://localhost:3000/process_create" method="post">
+			<p><input type="text" name="title" placeholder="title"></p>
+			<p>
+				<textarea name="description" placeholder="description"></textarea>
+			</p>
+			<p>
+				<input type="submit">
+			</p>
+		</form>`);
+		response.writeHead(200);
+		response.end(template);
 	} else {
 		response.writeHead(404);
 		response.end("Not found");
@@ -67,3 +70,4 @@ var app = http.createServer(function(request, response){
 });
 
 app.listen(3000);
+
